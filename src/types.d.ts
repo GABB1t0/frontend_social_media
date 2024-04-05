014179882c11f8c7c25964618730c220334fabfe
mvblanco99 <manuelvalentin1999@gmail.com>
2024-04-05 10:36:12 -0400

    Añadiendo valores al tipo endPointApi

--- src/types.d.ts
+++ src/types.d.ts
@@ -5,29 +5,25 @@ export type routesApp = '/'
     | '/signUp'
     | '/profile'
 
-export type endPointApi = `/user` 
+export type endPointApi = '/user'
     | `/user/view/${number}/images`
     | `/user/${number}/update`
     | `/posts/${number}`
-    | `/posts/store`
-    | `auth/login`
-    | `auth/register`
-
-
-// updatePost: (post:number) => `/posts/${post}/update/`,
-// destroyPost: (post:number) => `/posts/${post}/destroy`,
-// searchComment: (id_post:number) => `/post/${id_post}/comments`,
-// createComment: (idRecord:number,model:number) => `/post/comment/store/${idRecord}/${model}`,
-// updateComment: (id_comment:number) => `/post/comment/${id_comment}/update`,
-// destroyComment: (id_comment:number) => `/post/comment/${id_comment}/destroy`,
-// createReaction: (user:number, idRecord:number, model:number) => `/reaction/store/${user}/${idRecord}/${model}`,
-// destroyReaction: (reaction:number) => `/reaction/${reaction}/destroy`,
-// findFriends: (user:number) => `/friends/${user}/findFriends`,
-// findAllMyRequestFriend: () => '/friends/friendRequest',
-// findUsersToRecommend: () => '/friends/findUsersToRecommend',
-// sendRequestFriend: (recipient:number) => `/friends/${recipient}/request`,
-// acceptRequestFriend: (friendRequest:number) => `/friends/${friendRequest}/accept`,
-// destroyFriendshipRelationship: (user:number) => `/friends/${user}/destroy`,
-// login: () => 'auth/login',
-// signUp: () => 'auth/register',
-// verificationEmailSend:() => 'auth/email/verification-notification'
+    | '/posts/store'
+    | `/posts/${post}/update/`
+    | `/posts/${post}/destroy`
+    | `/post/${id_post}/comments`
+    | `/post/comment/store/${idRecord}/${model}`
+    | `/post/comment/${id_comment}/update`
+    | `/post/comment/${id_comment}/destroy`
+    | `/reaction/store/${user}/${idRecord}/${model}`
+    | `/reaction/${reaction}/destroy`
+    | `/friends/${user}/findFriends`
+    | '/friends/friendRequest'
+    | '/friends/findUsersToRecommend'
+    | `/friends/${recipient}/request`
+    | `/friends/${friendRequest}/accept`
+    | `/friends/${user}/destroy`
+    | 'auth/login'
+    | 'auth/register'
+    | 'auth/email/verification-notification'
