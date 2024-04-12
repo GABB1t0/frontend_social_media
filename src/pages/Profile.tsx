ebf5c7628a799bc205465e8efd075363e1b1dfee
mvblanco99 <manuelvalentin1999@gmail.com>
2024-04-12 12:02:11 -0400

    Impletamos logica de renderizado

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,35 +1,64 @@
-import Friends from './Profile-Pages/Friends';
-import Photos from './Profile-Pages/Photos';
-import About from './Profile-Pages/About';
-import TimeLine from './Profile-Pages/TimeLine';
-import { useParams } from 'react-router-dom';
-
+import { Outlet, useLocation, useParams } from 'react-router-dom';
 import ProfileBanner from '../components/Profile/ProfileBanner';
-import MenuSecundary from '../components/Profile/MenuSecundary';
+import { Header } from '../components/Header';
+import Timeline from './Profile-Pages/TimeLine';
+import { useEffect, useState } from 'react';
+import { client } from '../api/client';
+import {
+  ROUTES_API as routesApi
+} from '../config'
+import Loader from '../components/Loader';
 
 const Profile = () => {
 
-  let { section } = useParams<{ section?: string }>();
+  const [loading, setLoading] = useState(false)
+  const { id } = useParams();
+  const location = useLocation()
+  const clients = client()
+
+  useEffect(() => {
+    setLoading(true)
+    Promise.allSettled([
+      clients.get(routesApi.findUser(Number(id))),
+      clients.get(routesApi.findFriends(Number(id)))
+    ]).then( values => {
+      setLoading(false)
+      const [
+        {value : v1},
+        {value :v3},
+      ] = values
+    }).catch(error => {
+      console.log(error)
+      setLoading(false)
+    })
+  },[])
 
   return (
     <>
-      
-        
+      <Header navBlock={false}/>
       <div>
-        <ProfileBanner />
-          
-        {section != 'TimeLine' && <MenuSecundary section={section}/>}
-        
-        <div>
-          {section == 'TimeLine' && <TimeLine/> }
-          {section == 'Friends' && <Friends/> }
-          {section == 'Photos' && <Photos/> }
-          {section == 'About' && <About/> }
-        </div>        
+      {
+        loading
+          ?
+          <Loader/>
+          :
+          <>
+            <ProfileBanner />
+            {
+              location.pathname.includes('about') ||
+              location.pathname.includes('photos') ||
+              location.pathname.includes('friends')
+                ?   
+                <Outlet/>
+                :
+                <Timeline/>   
+            }
+          </>
+      }
       </div>
-      
     </>
   );
+  
 };
 
 export default Profile;
