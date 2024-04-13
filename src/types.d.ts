ccd15d4b0c4908f8c57acb6179cf249be204e532
mvblanco99 <manuelvalentin1999@gmail.com>
2024-04-12 21:51:48 -0400

    Modificando estrutura de los endpoint api

--- src/types.d.ts
+++ src/types.d.ts
@@ -6,23 +6,23 @@ export type RoutesApp = '/'
     | '/profile';
 
 export type EndPointApi = '/user'
-    | `find/${number}/user`
-    | `/user/view/${number}/images`
-    | `/user/${number}/update`
-    | `/posts/${number}?page=${number}`
+    | `find/${string}/user`
+    | `/user/view/${string}/images`
+    | `/user/${string}/update`
+    | `/posts/${string}?page=${number}`
     | '/posts/store'
-    | `/posts/${number}/update/`
-    | `/posts/${number}/destroy`
-    | `/post/${number}/comments`
-    | `/post/comment/${number}/update`
-    | `/post/comment/${number}/destroy`
-    | `/reaction/${number}/destroy`
-    | `/friends/${number}/findFriends`
+    | `/posts/${string}/update/`
+    | `/posts/${string}/destroy`
+    | `/post/${string}/comments`
+    | `/post/comment/${string}/update`
+    | `/post/comment/${string}/destroy`
+    | `/reaction/${string}/destroy`
+    | `/friends/${string}/findFriends`
     | '/friends/friendRequest'
     | '/friends/findUsersToRecommend'
-    | `/friends/${number}/request`
-    | `/friends/${number}/accept`
-    | `/friends/${number}/destroy`
+    | `/friends/${string}/request`
+    | `/friends/${string}/accept`
+    | `/friends/${string}/destroy`
     | 'auth/login'
     | 'auth/register'
     | 'auth/logout'
