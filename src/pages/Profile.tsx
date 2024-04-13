afbf0345869cadfce979727843e480e1fc7ff073
mvblanco99 <manuelvalentin1999@gmail.com>
2024-04-13 17:57:02 -0400

    Implementamos logica de renderizado

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -2,6 +2,7 @@ import { Outlet, useLocation, useParams } from 'react-router-dom';
 import { Header } from '../components/Header';
 import { Suspense, lazy } from 'react';
 import Loader from '../components/Loader';
+import { useVerifySesion } from '../hooks/useVerifySesion';
 
 const TimeLine = lazy( () => import('./Profile-Pages/TimeLine'))
 const ProfileBanner = lazy( () => import('../components/Profile/ProfileBanner'))
@@ -11,23 +12,29 @@ const Profile = () => {
   const { id } = useParams();
   const location = useLocation()
 
+  const { searchingToken } = useVerifySesion()
+
   return (
     <>
-      <Header navBlock={false}/>
-      <Suspense fallback={<div><Loader/></div>}>
-        <>
-            <ProfileBanner id={`${id}`} />
-            {
-              location.pathname.includes('about') ||
-              location.pathname.includes('photos') ||
-              location.pathname.includes('friends')
-                ?   
-                <Outlet/>
-                :
-                <TimeLine id={`${id}`}/>   
-            }
-          </>
-      </Suspense>
+      {
+        searchingToken && <div id='infiniteScroll' style={{overflow:'auto', height:'100vh'}}>
+          <Header navBlock={false}/>
+          <Suspense fallback={<div><Loader/></div>}>
+            <>
+              <ProfileBanner id={`${id}`} />
+              {
+                location.pathname.includes('about') ||
+                location.pathname.includes('photos') ||
+                location.pathname.includes('friends')
+                  ?   
+                  <Outlet/>
+                  :
+                  <TimeLine id={`${id}`}/>   
+              }
+            </>
+          </Suspense>
+        </div> 
+      }
     </>
   );
   
@@ -35,24 +42,3 @@ const Profile = () => {
 
 export default Profile;
 
-{/* <div>
-      {
-        !dataUser
-          ?
-          <Loader/>
-          :
-          <>
-            <ProfileBanner data={dataUser} />
-            {
-              
-              location.pathname.includes('about') ||
-              location.pathname.includes('photos') ||
-              location.pathname.includes('friends')
-                ?   
-                <Outlet/>
-                :
-                <Timeline/>   
-            }
-          </>
-      }
-      </div> */}
