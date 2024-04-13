2ed430ffb3adcb8fb15905a8263e412dac17c887
mvblanco99 <manuelvalentin1999@gmail.com>
2024-04-13 17:56:23 -0400

    Implementamos useVerifySession

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -5,16 +5,18 @@ import {
   nameCookieSessionApp,
   ROUTES_API as routesApi
 } from "../config";
-import { SingUpApiResponse  } from "../utils/SignUpApiResponse-types.ts";
 import { setCookie } from "../utils/cookies.ts";
 import { useEffect, useState } from "react";
 import { useRouter } from "../hooks/useRouter";
 import { Data } from "../utils/LoginApiResponse-types.ts";
+import { useVerifySesion } from "../hooks/useVerifySesion.ts";
 
 export const SignUp: React.FC = () => {
   const clients = client();
   const [logged, setLogged] = useState<boolean>(false)
   const { redirectToHome } = useRouter()
+
+  const { searchingToken } = useVerifySesion() 
 
   useEffect(()=>{
     if(logged) redirectToHome()
@@ -31,23 +33,26 @@ export const SignUp: React.FC = () => {
   }
 
   return (
-    <div className="h-screen w-full flex justify-center ">
-      <div className="bg-white w-72 h-96 flex flex-col  mx-auto my-auto justify-center gap-4">
-        <p className="text-2xl font-bold text-center">Registrarse</p>
-        <form onSubmit={handleSubmit} className="flex flex-col justify-center w-4/5 mx-auto gap-4">
-          <div className="flex flex-col">
-            <InputSesion name="name" type="text" placeholder="Ingrese Nombre" required={true} />
-            <InputSesion name="lastname" type="text" placeholder="Ingrese Apellido" required={true} />
-            <InputSesion name="email" type="email" placeholder="Ingrese Correo" required={true} />
-            <InputSesion name="password" type="password" placeholder="Ingrese Contraseña" required={true} />
-            <InputSesion name="password_confirmation" type="password" placeholder="Confirmar Contraseña" required={true} />
-          </div>
-          
-          <button type="submit" className="bg-[#fc6232] hover:bg-orange-700 text-white font-bold py-2 px-4 rounded" >Registrarse</button>
-          <p className="text-center">Ya tienes cuenta? <Link to="/" className="text-[#fc6232]">Inicia Sesion</Link></p>
-        </form>
-      </div>
-      
-    </div>
+    <>
+    { searchingToken &&
+         <div className="h-screen w-full flex justify-center ">
+         <div className="bg-white w-72 h-96 flex flex-col  mx-auto my-auto justify-center gap-4">
+           <p className="text-2xl font-bold text-center">Registrarse</p>
+           <form onSubmit={handleSubmit} className="flex flex-col justify-center w-4/5 mx-auto gap-4">
+             <div className="flex flex-col">
+               <InputSesion name="name" type="text" placeholder="Ingrese Nombre" required={true} />
+               <InputSesion name="lastname" type="text" placeholder="Ingrese Apellido" required={true} />
+               <InputSesion name="email" type="email" placeholder="Ingrese Correo" required={true} />
+               <InputSesion name="password" type="password" placeholder="Ingrese Contraseña" required={true} />
+               <InputSesion name="password_confirmation" type="password" placeholder="Confirmar Contraseña" required={true} />
+             </div>
+             
+             <button type="submit" className="bg-[#fc6232] hover:bg-orange-700 text-white font-bold py-2 px-4 rounded" >Registrarse</button>
+             <p className="text-center">Ya tienes cuenta? <Link to="/" className="text-[#fc6232]">Inicia Sesion</Link></p>
+           </form>
+         </div>
+       </div>
+      }
+    </>
   );
 }
