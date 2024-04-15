a53661cf0ac0bb3789781518e5653af7a215c905
mvblanco99 <manuelvalentin1999@gmail.com>
2024-04-15 19:45:36 -0400

    Modificamos estructura del fetchData

--- src/pages/Profile-Pages/TimeLine.tsx
+++ src/pages/Profile-Pages/TimeLine.tsx
@@ -5,8 +5,15 @@ import { ROUTES_API } from '../../config';
 import { Data, Datum, Posts } from '../../utils/SearchPostProfileApiResponse'
 import { EndPointApi } from '../../types';
 import { Post } from '../../components/post/Post';
+import { CreatePost } from '../../components/post/CreatePost';
 
 type Props = { id:string }
+
+type PropsFetchData = { 
+  page:number|null, 
+  url:EndPointApi|null, 
+  signal?:AbortSignal
+}
 
 const InfoComponent = lazy(() => import('../../components/infocomponent/InfoComponent'))
 
@@ -15,26 +22,35 @@ const Timeline = ({id}:Props) => {
   const array: Datum[] = []
 
   const [page,setPage] = useState<Data>();
-  const [items,setItems] = useState(array);
-  const clients = client() 
+  const [items,setItems] = useState<Datum[]>([]);
+  const [hasMore, setHasMore] = useState(true)
+  const clients = client()
 
-  const getList = (page:number|null, url:EndPointApi|null, signal?:AbortSignal):void => {
+  const fetchData = ({page, url, signal}:PropsFetchData) => {
+    
+    const paginationDefault = 3;
     let uri = 
       page === null
         ? url
-        : ROUTES_API.searchPostsUser(`${id}`,page);
+        : ROUTES_API.searchPostsUser(`${id}`,paginationDefault,page);
 
     if(uri === null) return   
 
-    const request = signal != undefined ? clients.get(uri,signal) : clients.get(uri)
+    const request = signal != undefined ? clients.get(uri,signal) : clients.get(uri);
+
     request
     .then(response  => {
       const data = response.data as Data
       const posts = (response.data.posts) as Posts
-      let newDataPosts = posts.data
-      setItems([...items, ...newDataPosts])
-      setPage(data)
-    })
+      console.log(data)
+      if(posts.data.length === 0){ 
+        setHasMore(false)
+      }else{
+        let newDataPosts = posts.data
+        setItems([...items, ...newDataPosts])
+        setPage(data)
+      }
+    });
   }
 
   const acomodarUrl = (url:string|null|undefined) => {
@@ -46,7 +62,13 @@ const Timeline = ({id}:Props) => {
   useEffect(()=>{
     const abortcontroller = new AbortController()
     const signal = abortcontroller.signal
-    getList(1,null,signal)
+    console.log('renderice useeffect')
+    fetchData({
+      page:1,
+      url:null,
+      signal:signal
+    })
+
     return () => {
       abortcontroller.abort()
     }
@@ -57,16 +79,30 @@ const Timeline = ({id}:Props) => {
       {
         <InfiniteScroll
           dataLength={items.length} //This is important field to render the next data
-          next={() => {
-            if(page?.posts.next_page_url === null) return
-            getList(null,acomodarUrl(page?.posts.next_page_url))
+          next={ () => { 
+            fetchData({
+              page:null,
+              url:acomodarUrl(page?.posts?.next_page_url)
+            })
           }}
-          hasMore={true}
+          hasMore={hasMore}
           scrollableTarget='infiniteScroll'
-          loader={page?.posts.next_page_url !== null ? 'cargando' : ''}
+          loader={
+            <div className='flex justify-end sm:w-11/12 mx-auto my-3 md:gap-6'>
+              <div className="w-full md:w-[58%] overflow-y-auto">
+                <Post description='Cargando'/>
+                <br />
+                <Post description='Cargando'/>
+                <br />
+              </div>
+            </div>
+          }
+          endMessage={<p>Ya se cargaron todos los datos</p>}
+          scrollThreshold="100px"
+          
           >
-            <div className='flex sm:w-11/12 mx-auto my-3 md:gap-6  '>
-              <aside className="md:w-[40%] sticky top-20 h-4/5 z-[49]">
+            <div className='flex sm:w-11/12 mx-auto my-3 md:gap-6'>
+              <aside className="md:w-[40%] sticky top-0 h-4/5 z-[49]">
                 <div className="hidden md:flex flex-col gap-4 ">
                   <Suspense>
                     <InfoComponent/>
@@ -75,11 +111,15 @@ const Timeline = ({id}:Props) => {
                 </div>
               </aside>
               <main className="w-full md:w-[60%] overflow-y-auto">
-                {
-                  items.map(item => (
-                    <><Post key={item.id}/><br /></>
-                  ))
-                }
+                  <div>
+                    <CreatePost/>
+                    <br />
+                    {
+                      items.map((item) => (
+                        <Post key={item.id} description={`${item.id}`}/>
+                      ))
+                    }
+                  </div>
               </main>
             </div> 
           </InfiniteScroll>
