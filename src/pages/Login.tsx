f0f93ae41ac7c7699e8d47c1083af55437cbbc07
mvblanco99 <manuelvalentin1999@gmail.com>
2024-04-24 19:00:49 -0400

    Implementado hook useVerifySession

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,9 +1,10 @@
 import { InputSesion } from "../components/ui/InputSession";
 import { client } from "../api/client";
-import { Link } from 'react-router-dom';
+import { Link, useLoaderData } from 'react-router-dom';
 import { FC, useEffect, useState } from "react";
 import { setCookie } from "../utils/cookies";
 import { 
+  SUPPORTED_ROUTES,
   nameCookieSessionApp, 
   ROUTES_API as routesApi
 } from '../config';
@@ -11,12 +12,13 @@ import { Data } from "../utils/LoginApiResponse-types";
 import { useRouter } from "../hooks/useRouter";
 import { useVerifySesion } from "../hooks/useVerifySesion";
 
-export const Login: FC = () => {
+const Login: FC = () => {
+
   const clients = client();
   const [logged, setLogged] = useState<boolean>(false)
   const { redirectToHome } = useRouter()
 
-  const { searchingToken } = useVerifySesion() 
+  const { searchingToken } = useVerifySesion();
 
   useEffect(()=>{
     if(logged) redirectToHome()
@@ -35,9 +37,8 @@ export const Login: FC = () => {
   
   return (
     <>
-      {
-        searchingToken && 
-          <div className="h-screen w-full flex justify-center ">
+      { searchingToken &&
+        <div className="h-screen w-full flex justify-center ">
           <div className="bg-white w-72 h-96 flex flex-col  mx-auto my-auto justify-center gap-4">
             <p className="text-2xl font-bold text-center">Iniciar Sesion</p>
             <form onSubmit={handleSubmit} className="flex flex-col justify-center w-4/5 mx-auto gap-4">
@@ -48,7 +49,7 @@ export const Login: FC = () => {
                 <InputSesion name="password" type="password" placeholder="Ingrese Contraseña" required={true} />
               </div>
               <button type="submit" className="bg-[#fc6232] hover:bg-orange-700 text-white font-bold py-2 px-4 rounded" >Iniciar Sesion</button>
-              <p className="text-center">No tienes cuenta? <Link to="/SignUp" className="text-[#fc6232]">Registrate</Link></p>
+              <p className="text-center">No tienes cuenta? <Link to={SUPPORTED_ROUTES.signUp()} className="text-[#fc6232]">Registrate</Link></p>
             </form>
           </div>
         </div>
@@ -56,3 +57,5 @@ export const Login: FC = () => {
     </>
   );
 }
+
+export default Login;
--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -2,6 +2,7 @@ import { client} from "../api/client";
 import { InputSesion } from "../components/ui/InputSession";
 import { Link } from "react-router-dom";
 import { 
+  SUPPORTED_ROUTES,
   nameCookieSessionApp,
   ROUTES_API as routesApi
 } from "../config";
@@ -11,12 +12,12 @@ import { useRouter } from "../hooks/useRouter";
 import { Data } from "../utils/LoginApiResponse-types.ts";
 import { useVerifySesion } from "../hooks/useVerifySesion.ts";
 
-export const SignUp: React.FC = () => {
+const SignUp: React.FC = () => {
   const clients = client();
   const [logged, setLogged] = useState<boolean>(false)
   const { redirectToHome } = useRouter()
 
-  const { searchingToken } = useVerifySesion() 
+  const { searchingToken } = useVerifySesion();
 
   useEffect(()=>{
     if(logged) redirectToHome()
@@ -25,11 +26,16 @@ export const SignUp: React.FC = () => {
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const formData = new FormData(e.currentTarget);
-    const res = await clients.post(routesApi.signUp(), formData);
-    const  data  = res.data as Data
-    const { token } = data
-    setCookie(nameCookieSessionApp,token,1000)
-    setLogged(true)
+
+    try {
+      const res = await clients.post(routesApi.signUp(), formData);
+      const  data  = res.data as Data
+      const { token } = data
+      setCookie(nameCookieSessionApp,token,1000)
+      setLogged(true)
+    } catch (error) {
+      console.log(error)
+    }
   }
 
   return (
@@ -40,15 +46,15 @@ export const SignUp: React.FC = () => {
            <p className="text-2xl font-bold text-center">Registrarse</p>
            <form onSubmit={handleSubmit} className="flex flex-col justify-center w-4/5 mx-auto gap-4">
              <div className="flex flex-col">
-               <InputSesion name="name" type="text" placeholder="Ingrese Nombre" required={true} />
-               <InputSesion name="lastname" type="text" placeholder="Ingrese Apellido" required={true} />
-               <InputSesion name="email" type="email" placeholder="Ingrese Correo" required={true} />
-               <InputSesion name="password" type="password" placeholder="Ingrese Contraseña" required={true} />
-               <InputSesion name="password_confirmation" type="password" placeholder="Confirmar Contraseña" required={true} />
+               <InputSesion name="name" type="text" placeholder="Ingrese Nombre" required={false} />
+               <InputSesion name="lastname" type="text" placeholder="Ingrese Apellido" required={false} />
+               <InputSesion name="email" type="email" placeholder="Ingrese Correo" required={false} />
+               <InputSesion name="password" type="password" placeholder="Ingrese Contraseña" required={false} />
+               <InputSesion name="password_confirmation" type="password" placeholder="Confirmar Contraseña" required={false} />
              </div>
              
              <button type="submit" className="bg-[#fc6232] hover:bg-orange-700 text-white font-bold py-2 px-4 rounded" >Registrarse</button>
-             <p className="text-center">Ya tienes cuenta? <Link to="/" className="text-[#fc6232]">Inicia Sesion</Link></p>
+             <p className="text-center">Ya tienes cuenta? <Link to={SUPPORTED_ROUTES.login()} className="text-[#fc6232]">Inicia Sesion</Link></p>
            </form>
          </div>
        </div>
@@ -56,3 +62,5 @@ export const SignUp: React.FC = () => {
     </>
   );
 }
+
+export default SignUp;
