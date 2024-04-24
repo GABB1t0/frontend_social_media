5eaf9a2453ca439d9973a23aa9fc669e1987e694
mvblanco99 <manuelvalentin1999@gmail.com>
2024-04-24 19:04:14 -0400

    Implementamos hook para gestionar las instacias de hook de redux

---
+++ src/hooks/useReduxHook.ts
@@ -0,0 +1,12 @@
+import { useDispatch, useSelector } from "react-redux"
+
+const useReduxHook = () => {
+    const myUseSelector = useSelector;
+    const dispatch = useDispatch()
+    return {
+        myUseSelector,
+        dispatch
+    }
+}
+
+export default useReduxHook
