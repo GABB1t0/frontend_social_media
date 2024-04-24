14adba3bed7057951a00840ef44b989bb2cf6d7d
mvblanco99 <manuelvalentin1999@gmail.com>
2024-04-24 19:28:04 -0400

    Implementamos el hook useLoaderData

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,55 +9,53 @@ import {
   nameCookieSessionApp, 
   ROUTES_API as routesApi
 } from '../config';
-import { useNavigate } from "react-router-dom";
+import { useLoaderData, useNavigate } from "react-router-dom";
 
 
-export const Home = () => {
-  const [showHome,setShowHome] = useState(false)
+const Home = () => {
+  
+  const dataUserLogged = useLoaderData();
+  console.log(dataUserLogged)
 
-  const clients = client();
-  const navigate = useNavigate();
+  // const clients = client();
+  // const navigate = useNavigate();
 
   
   //useEffect
   
-    const verifyUser = async()=>{
+    // const verifyUser = async()=>{
       
-        const resVerified = await clients.get(routesApi.userLogged())
-      
+    //     const resVerified = await clients.get(routesApi.userLogged())
+    //     console.log(JSON.stringify(resVerified))
       
     
-      setShowHome(true)
-    }
-    verifyUser();
+    //   setShowHome(true)
+    // }
+    // verifyUser();
 
   return (
-    showHome &&
-    
     <>
-
-    
-    <Header navBlock={false} />
-    <div className='container flex justify-center sm:w-11/12 mx-auto my-3 md:gap-6 lg:gap-6 xl:w-4/5'>
-      <aside className="lg:w-[25%] sticky top-20 h-4/5 z-[49]">
-        <div className="hidden md:flex flex-col gap-4 ">
-          
-          <ImageProfileHome />
-          <InfoComponent/>
-        </div>
-      </aside>
-      <main className="w-full lg:w-[50%] overflow-y-auto mt-[68px]">
-        <Feed/>
-      </main>
-      <aside className="lg:w-[25%] h-4/5 sticky top-20 right-0  z-40">
-        <div className="hidden lg:flex flex-col gap-4">
-          <InfoComponent/>
-          <InfoComponent/>
-        </div>
-      </aside>
-      
-    </div>
+      <Header navBlock={false} />
+      <div className='container flex justify-center sm:w-11/12 mx-auto my-3 md:gap-6 lg:gap-6 xl:w-4/5'>
+        <aside className="lg:w-[25%] sticky top-20 h-4/5 z-[49]">
+          <div className="hidden md:flex flex-col gap-4 ">
+            
+            <ImageProfileHome />
+            <InfoComponent/>
+          </div>
+        </aside>
+        <main className="w-full lg:w-[50%] overflow-y-auto mt-[68px]">
+          <Feed/>
+        </main>
+        <aside className="lg:w-[25%] h-4/5 sticky top-20 right-0  z-40">
+          <div className="hidden lg:flex flex-col gap-4">
+            <InfoComponent/>
+            <InfoComponent/>
+          </div>
+        </aside>
+      </div>
     </>
-    
   );
 }
+
+export default Home;
