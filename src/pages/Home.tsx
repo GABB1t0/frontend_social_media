a031bc78e69c72edd6fa46a0b42948688b4bf9d2
mvblanco99 <manuelvalentin1999@gmail.com>
2024-04-25 12:22:14 -0400

    Implementando lazy loading

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,10 @@
-
-import { useEffect, useState } from "react";
+import { Suspense, lazy } from "react";
 import { Feed } from "../components/Feed"
-import { Header } from "../components/Header";
 import { ImageProfileHome } from "../components/ImageProfileHome"
 import  InfoComponent  from "../components/infocomponent/InfoComponent"
-import { client } from "../api/client";
-import { 
-  nameCookieSessionApp, 
-  ROUTES_API as routesApi
-} from '../config';
-import { useLoaderData, useNavigate } from "react-router-dom";
+import { useLoaderData } from "react-router-dom";
 
+const Header = lazy(() => import('../components/Header'));
 
 const Home = () => {
   
@@ -19,41 +13,37 @@ const Home = () => {
 
   // const clients = client();
   // const navigate = useNavigate();
-
-  
   //useEffect
-  
     // const verifyUser = async()=>{
-      
     //     const resVerified = await clients.get(routesApi.userLogged())
     //     console.log(JSON.stringify(resVerified))
-      
-    
     //   setShowHome(true)
     // }
     // verifyUser();
 
   return (
     <>
-      <Header navBlock={false} />
-      <div className='container flex justify-center sm:w-11/12 mx-auto my-3 md:gap-6 lg:gap-6 xl:w-4/5'>
-        <aside className="lg:w-[25%] sticky top-20 h-4/5 z-[49]">
-          <div className="hidden md:flex flex-col gap-4 ">
-            
-            <ImageProfileHome />
-            <InfoComponent/>
+      <Suspense>
+        <Header navBlock={false} />
+          <div className='container flex justify-center sm:w-11/12 mx-auto my-3 md:gap-6 lg:gap-6 xl:w-4/5'>
+            <aside className="lg:w-[25%] sticky top-20 h-4/5 z-[49]">
+              <div className="hidden md:flex flex-col gap-4 ">
+                
+                <ImageProfileHome />
+                <InfoComponent/>
+              </div>
+            </aside>
+            <main className="w-full lg:w-[50%] overflow-y-auto mt-[68px]">
+              <Feed/>
+            </main>
+            <aside className="lg:w-[25%] h-4/5 sticky top-20 right-0  z-40">
+              <div className="hidden lg:flex flex-col gap-4">
+                <InfoComponent/>
+                <InfoComponent/>
+              </div>
+            </aside>
           </div>
-        </aside>
-        <main className="w-full lg:w-[50%] overflow-y-auto mt-[68px]">
-          <Feed/>
-        </main>
-        <aside className="lg:w-[25%] h-4/5 sticky top-20 right-0  z-40">
-          <div className="hidden lg:flex flex-col gap-4">
-            <InfoComponent/>
-            <InfoComponent/>
-          </div>
-        </aside>
-      </div>
+      </Suspense>
     </>
   );
 }
