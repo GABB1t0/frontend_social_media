e410073dd7e2c913c33be087e1547dfeefd062ab
mvblanco99 <manuelvalentin1999@gmail.com>
2024-04-25 12:28:30 -0400

    Implementando loader

---
+++ src/components/LoaderPages.tsx
@@ -0,0 +1,9 @@
+import LoaderCss from './LoaderPages.module.css';
+
+const LoaderPages = () => {
+    <div className={LoaderCss.container}>
+        <div className={LoaderCss.loader}></div>
+    </div>
+}
+
+export default LoaderPages
